fdfbe5ad73798c7a4538415807efc3d04b778890
Tim Spinks <tim@ninthlink.com>
2017-07-06 12:24:08 -0700

    init

---
+++ README.md
@@ -0,0 +1,23 @@
+Bootstrap Gulp 4 Boilerplate
+============================
+
+A boilerplate Bootstrap project, featuring Gulp 4 and NPM package management.
+
+## Quick Start ##
+
+- Requires Gulp 4 and NPM
+- Download from GitHub and install by running `npm install`
+- Static assets are stored in `src/img/` and `src/fonts`
+- Add custom SASS to `src/_custom.scss`
+- Add custom JavaScript to `src/js/custom.js`
+- Assets are combined and minified and stored in `dist/` for deployment
+- To build assets for production run `gulp build`
+
+## License ##
+
+Code released under MIT license. Additional license information can be found at:
+
+- [Font Awesome](http://fontawesome.io/license/)
+- [jQuery](https://jquery.org/license/)
+- [Bootstrap](http://getbootstrap.com/getting-started/#license-faqs)
+ 
---
+++ gulpfile.js
@@ -0,0 +1,108 @@
+// Defining base pathes
+var paths = {
+    bower: './bower_components/',
+    node: './node_modules/',
+    dist: './dist/',
+    src: ['./src/'],
+    fonts: {
+        src: './src/fonts/**/*.{ttf,woff,woff2,eof,svg}',
+        dest: './dist/fonts/'
+    },
+    images: {
+        src: './src/img/**/*.{fpx,gif,jpg,jpeg,jif,jfif,jp2,jpx,j2k,j2c,pcd,pdf,png,tif,tiff}',
+        dest: './dist/img/'
+    },
+    scripts: {
+        src: './src/js/**/*.js',
+        dest: './dist/js/'
+    },
+    styles: {
+        src: './src/sass/',
+        dest: './dist/css/'
+    }
+};
+
+var gulp = require('gulp');
+var addsrc = require('gulp-add-src');
+var babel = require('gulp-babel');
+var cleanCSS = require('gulp-clean-css');
+var concat = require('gulp-concat');
+var del = require('del');
+var imagemin = require('gulp-imagemin');
+var notify = require('gulp-notify');
+var plumber = require('gulp-plumber');
+var rename = require('gulp-rename');
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+var watch = require('gulp-watch');
+
+function assets() {
+    return gulp.src(['src/**/*', '!src/fonts', '!src/fonts/**', '!src/img', '!src/img/**', '!src/js', '!src/js/**', '!src/sass', '!src/sass/**'])
+        .pipe(gulp.dest(paths.dist));
+}
+
+function clean() {
+    return del(paths.dist);
+};
+
+function fonts() {
+    var fonts = [
+        paths.node + 'font-awesome/fonts/**/*.{ttf,woff,woff2,eof,svg}',
+        paths.node + 'bootstrap-sass/assets/fonts/**/*.{ttf,woff,woff2,eof,svg}',
+        paths.fonts.src
+    ];
+    return gulp.src(fonts)
+        .pipe(gulp.dest(paths.fonts.dest));
+};
+
+function images(){
+    return gulp.src(paths.images.src)
+        .pipe(imagemin())
+        .pipe(gulp.dest(paths.images.dest));
+};
+
+function scripts() {
+    var scripts = [
+        paths.node + 'jquery/dist/jquery.js',
+        paths.node + 'bootstrap-sass/assets/javascripts/**/*.js',
+        paths.scripts.src
+    ];
+    return gulp.src(scripts)
+        .pipe(babel())
+        .pipe(uglify())
+        .pipe(concat('scripts.min.js'))
+        .pipe(gulp.dest(paths.scripts.dest));
+};
+
+function styles() {
+    return gulp.src(paths.styles.src + 'app.scss')
+        .pipe(plumber())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(cleanCSS({
+            compatibility: 'ie8'
+        }))
+        .pipe(rename({
+            basename: 'styles',
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest(paths.styles.dest));
+};
+
+function watch() {
+    gulp.watch(paths.sass.src + '**/*.scss', sass);
+    gulp.watch(paths.scripts.src, scripts);
+};
+
+exports.assets = assets;
+exports.clean = clean;
+exports.fonts = fonts;
+exports.images = images;
+exports.scripts = scripts;
+exports.styles = styles;
+exports.watch = watch;
+
+var init = gulp.parallel(assets, fonts, images);
+var build = gulp.series(clean, gulp.parallel(assets, fonts, images, scripts, styles));
+
+gulp.task('init', init);
+gulp.task('build', build);
